c99e2c442ea2f960c9dfc3117990a3cd530e8cbb
Poluvyanov Ilya <poluvyanov.i@mail.ru>
2019-09-12 22:39:18 +0600

    Init application

---
+++ app/Console/Kernel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console;
+
+use App\Console\Commands\TestCommand;
+use App\Jobs\TestJob;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        TestCommand::class
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+//        $schedule->command('command:test')->everyMinute();
+
+        $schedule->command('telescope:prune --hours=48')->daily();
+        // $schedule->command('inspire')
+        //          ->hourly();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
